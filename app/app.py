983b7096ed947cc894a567c3e2e6d3e7ab869a7a
Aswin Lal M <aswnss.m@gmail.com>
2021-08-24 18:07:43 +0530

    index page is mobile ready

--- app/app.py
+++ app/app.py
@@ -1,10 +1,18 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        name = request.form.get('Name')
+        email = request.form.get('Email')
+        return redirect('/'+name)
+    else:
+        return render_template('index.html')
 
+@app.route('/<string:name>')
+def predict(name):
+    return "Hello " + name
 if __name__=='__main__':
     app.run(debug=True)
