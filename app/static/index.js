e6655c33a454fb764857bedda6e7fc23a2193f6f
Aswin Lal M <aswnss.m@gmail.com>
2021-08-24 12:42:25 +0530

    stated building form

--- app/static/index.js
+++ app/static/index.js
@@ -1,6 +1,8 @@
 function reveal(){
     var intro = document.getElementById('intro');
+    var content = document.getElementById('content');
     intro.style.transform = 'translateX(-100%)';
+    content.style.transform = 'translateX(0)';
     // intro.style.-webkit-transform = "translateX(-100%)";
     // intro.style.-moz-transform = "translateX(-100%)";
     // intro.style.-o-transform = "translateX(-100%)";
